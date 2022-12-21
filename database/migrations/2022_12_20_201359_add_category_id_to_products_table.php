1312c7622036db17b6889530615a39344646f807
Corax73 <ramaz999@inbox.ru>
2022-12-21 23:40:46 +0300

    debug migration, route and controller

--- database/migrations/2022_12_20_201359_add_category_id_to_products_table.php
+++ database/migrations/2022_12_20_201359_add_category_id_to_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned()->nullable();;
             $table->foreign('category_id')
                 ->references('id')
                 ->on('categories')
